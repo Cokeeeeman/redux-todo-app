2239dba478a193be34fbdd31bdaf992bab213e3e
Ziyue Chen(Max) <ziyuechen@yahoo.com>
2018-04-05 11:30:58 -0700

    Throttle state saving rate to no more than 1s a time

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import todoApp from "./reducers/todoApp";
 import { createStore } from "redux";
 import { loadState, saveState } from './util/localStorage';
+import throttle from 'lodash/throttle';
 
 const preloadedState = loadState();
 
@@ -9,10 +10,12 @@ const store = createStore(
   preloadedState
 );
 
-store.subscribe(() => {
+const saveStateLocal = () => {
   saveState({
     todos: store.getState().todos
   });
-});
+};
+
+store.subscribe(throttle(saveStateLocal, 1000));
 
 export default store;
